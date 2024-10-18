7033a62532ff761489ef51228d26750f69c9c6ea
AyushVaibhav3 <147472147+AyushVaibhav3@users.noreply.github.com>
2024-10-18 06:30:44 +0000

    Error Resolved

--- Login_API_Integration/src/app/Pages/login/login.component.ts
+++ Login_API_Integration/src/app/Pages/login/login.component.ts
@@ -1,22 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
-signUpMode: boolean = false;
+//   loginObj:any ={
+//     "EmailId": "",
+//     "Password": ""
+//   }
 
-  OnSignUpClick(){
-    this.signUpMode = true ;
-  }
 
-  OnSigninClick(){
-    this.signUpMode = false ;
-  }
+// signUpMode: boolean = false;
+
+//   OnSignUpClick(){
+//     this.signUpMode = true ;
+//   }
+
+//   OnSigninClick(){
+//     this.signUpMode = false ;
+//   }
 
 }
